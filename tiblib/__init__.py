271b016a0320618dfd28074f96606ee3bf168c25
akatief <f.tib29@gmail.com>
2023-01-21 17:37:44 +0100

    feat: implement Gaussianizer transformer

--- tiblib/__init__.py
+++ tiblib/__init__.py
@@ -1,6 +1,6 @@
 from .utils import covariance
 from .utils import train_test_split, logpdf_GAU_ND, logpdf_GAU_ND_multi_comp, logpdf_GMM
-from .utils import load_iris_binary, load_iris_multiclass
+from .utils import load_iris_binary, load_iris_multiclass, load_wine
 from .utils import TransformerBase, ClassifierBase
 
 
@@ -13,6 +13,7 @@ __all__ = [
     'train_test_split',
     'load_iris_binary',
     'load_iris_multiclass',
+    'load_wine',
     'TransformerBase',
     'ClassifierBase',
     'logpdf_GAU_ND',
--- tiblib/preprocessing/__init__.py
+++ tiblib/preprocessing/__init__.py
@@ -1,9 +1,11 @@
 from .pca import PCA
 from .lda import LDA
 from .kfold import Kfold
+from .gaussianizer import Gaussianizer
 
 __all__ = [
     'PCA',
     'LDA',
     'Kfold',
+    'Gaussianizer'
 ]
