62abcee47e6b119d21304a8abbfa33057cb8bd28
akatief <f.tib29@gmail.com>
2023-01-23 00:35:37 +0100

    refactor: move Pipeline in file pipeline.py

--- tiblib/utils.py
+++ tiblib/utils.py
@@ -1,7 +1,6 @@
 import json
 import warnings
 from abc import abstractmethod, ABC
-from typing import Union
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -89,38 +88,6 @@ class CVMinDCF:
             if score < self.best_score:
                 self.best_score = score
         return self.best_score
-
-
-class Pipeline(ClassifierBase):
-    '''
-    Pipeline class containing n transformers and one final classifier.
-    Used to standardize preprocessing and avoid data leakage
-    '''
-    def __init__(self, transformers: Union[list, TransformerBase] , classifier: ClassifierBase):
-        if len(transformers) == 1 and type(transformers) is not list:
-            self.transformers = [transformers]
-        else:
-            self.transformers = transformers
-        self.classifier = classifier
-
-    def fit(self, X, y):
-        X_transf = X
-        for t in self.transformers:
-            X_transf = t.fit_transform(X_transf)
-        self.classifier.fit(X_transf,y)
-        return self
-
-    def predict(self, X):
-        X_transf = X
-        for t in self.transformers:
-            X_transf = t.transform(X_transf)
-        return self.classifier.predict(X_transf)
-
-    def predict_scores(self, X, get_ratio=False):
-        X_transf = X
-        for t in self.transformers:
-            X_transf = t.transform(X_transf)
-        return self.classifier.predict_scores(X_transf, get_ratio=get_ratio)
 
 
 def train_test_split(X, y, test_size, seed=0):
