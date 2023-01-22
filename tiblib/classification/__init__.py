f7ebd56fd8ee92f4f72ca8309b0b0e18979cc4cb
akatief <f.tib29@gmail.com>
2023-01-23 00:42:27 +0100

    fix: add pipeline to init

--- tiblib/classification/__init__.py
+++ tiblib/classification/__init__.py
@@ -1,11 +1,13 @@
 from .logistic_regression import BinaryLogisticRegression, LogisticRegression
 from .gaussian_classifier import GaussianClassifier
 from .gaussian_mixture import GaussianMixtureModel, GaussianMixtureClassifier
+from .pipeline import Pipeline
 
 __all__ = [
     'BinaryLogisticRegression',
     'LogisticRegression',
     'GaussianClassifier',
     'GaussianMixtureModel',
-    'GaussianMixtureClassifier'
+    'GaussianMixtureClassifier',
+    'Pipeline'
 ]
