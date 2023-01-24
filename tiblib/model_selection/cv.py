b90b4fac130c9a64091ad997bf670522069eca6a
akatief <f.tib29@gmail.com>
2023-01-24 11:04:59 +0100

    refactor: write CVs as methods instead of classes

--- tiblib/model_selection/cv.py
+++ tiblib/model_selection/cv.py
@@ -34,89 +34,63 @@ class Kfold:
         return folds
 
 
-class CVMinDCF:
-    def __init__(self, model, K=5, pi=.5):
-        '''
-        Runs KFold CV on a given model with min DCF as a metric.
+def CVMinDCF(model, X, y, K=5, pi=.5):
+    if X.shape[0] < X.shape[1]:
+        warnings.warn(f'Samples in X should be rows. Are you sure the dataset is not transposed? Size: {X.shape}')
+    best_score = np.inf
+    best_act_score = None
+    scores = []
+    best_model = None
 
-        :param model: model to cross-validate
-        :param K: number of folds
-        :param pi: prior over model scores
-        '''
-        self.model = model
-        self.K = K
-        self.scores = []
-        self.best_score = 1
-        self.best_model = None
-        self.act_score = None
-        self.pi = pi
+    n = X.shape[0]
+    indices = np.arange(n)
+    np.random.shuffle(indices)
+    fold_size = int(n / K)
+    for i in range(K):
+        val_indices = indices[i * fold_size:(i + 1) * fold_size]
+        train_indices = np.setdiff1d(indices, val_indices)
+        X_train, y_train = X[train_indices], y[train_indices]
+        X_val, y_val = X[val_indices], y[val_indices]
+        model.fit(X_train, y_train)
 
-    def score(self, X, y):
-        if X.shape[0] < X.shape[1]:
-            warnings.warn(f'Samples in X should be rows. Are you sure the dataset is not transposed? Size: {X.shape}')
-        n = X.shape[0]
-        indices = np.arange(n)
-        np.random.shuffle(indices)
-        fold_size = int(n / self.K)
-        for i in range(self.K):
-            val_indices = indices[i * fold_size:(i + 1) * fold_size]
-            train_indices = np.setdiff1d(indices, val_indices)
-            X_train, y_train = X[train_indices], y[train_indices]
-            X_val, y_val = X[val_indices], y[val_indices]
-            self.model.fit(X_train, y_train)
-
-            val_scores = self.model.predict_scores(X_val, get_ratio=True)
-            score, _ = min_detection_cost_func(val_scores, y_val, pi=self.pi)
-            act_score = detection_cost_func(val_scores, y_val, pi=self.pi)
-            self.scores.append(score)
-            if score < self.best_score:
-                self.best_score = score
-                self.act_score = act_score
-                self.best_model = deepcopy(self.model)
-        return self.best_score, self.best_model, self.act_score
+        val_scores = model.predict_scores(X_val, get_ratio=True)
+        score, _ = min_detection_cost_func(val_scores, y_val, pi=pi)
+        act_score = detection_cost_func(val_scores, y_val, pi=pi)
+        scores.append(score)
+        if score < best_score:
+            best_score = score
+            best_act_score = act_score
+            best_model = deepcopy(model)
+    return best_score, best_act_score, best_model
 
 
-class CVCalibration:
-    def __init__(self, model, _lambda=1e-3, K=5, pi=.5):
-        '''
-        Runs KFold CV on a given model with min DCF as a metric.
-        Calibrates the scores using a LogisticRegression with
-        a given lambda
+def CVCalibration(model, X, y, K=5, pi=.5, _lambda=1e-3):
+    if X.shape[0] < X.shape[1]:
+        warnings.warn(f'Samples in X should be rows. Are you sure the dataset is not transposed? Size: {X.shape}')
+    best_score = np.inf
+    best_act_score = None
+    scores = []
+    best_model = None
 
-        :param model: model to cross-validate
-        :param _lambda: logreg hyperparameter during calibration
-        :param K: number of folds
-        :param pi: prior over model scores
-        '''
-        self.model = model
-        self.K = K
-        self.scores = []
-        self.best_score = 1
-        self.best_model = None
-        self.pi = pi
-        self._lambda = _lambda
+    n = X.shape[0]
+    indices = np.arange(n)
+    np.random.shuffle(indices)
+    fold_size = int(n / K)
+    for i in range(K):
+        val_indices = indices[i * fold_size:(i + 1) * fold_size]
+        train_indices = np.setdiff1d(indices, val_indices)
+        X_train, y_train = X[train_indices], y[train_indices]
+        X_val, y_val = X[val_indices], y[val_indices]
+        model.fit(X_train, y_train)
 
-    def score(self, X, y):
-        if X.shape[0] < X.shape[1]:
-            warnings.warn(f'Samples in X should be rows. Are you sure the dataset is not transposed? Size: {X.shape}')
-        n = X.shape[0]
-        indices = np.arange(n)
-        np.random.shuffle(indices)
-        fold_size = int(n / self.K)
-        for i in range(self.K):
-            val_indices = indices[i * fold_size:(i + 1) * fold_size]
-            train_indices = np.setdiff1d(indices, val_indices)
-            X_train, y_train = X[train_indices], y[train_indices]
-            X_val, y_val = X[val_indices], y[val_indices]
-            self.model.fit(X_train, y_train)
+        val_scores = model.predict_scores(X_val, get_ratio=True)
+        cal_scores = calibrate(val_scores, y_val, _lambda, pi)
 
-            val_scores = self.model.predict_scores(X_val, get_ratio=True)
-            cal_scores = calibrate(val_scores, y_val, self._lambda, self.pi)
-            score, _ = min_detection_cost_func(cal_scores, y_val, pi=self.pi)
-            act_score = detection_cost_func(val_scores, y_val, pi=self.pi)
-            self.scores.append(score)
-            if score < self.best_score:
-                self.best_score = score
-                self.act_score = act_score
-                self.best_model = deepcopy(self.model)
-        return self.best_score, self.best_model, self.act_score
+        score, _ = min_detection_cost_func(cal_scores, y_val, pi=pi)
+        act_score = detection_cost_func(val_scores, y_val, pi=pi)
+        scores.append(score)
+        if score < best_score:
+            best_score = score
+            best_act_score = act_score
+            best_model = deepcopy(model)
+    return best_score, best_act_score, best_model
